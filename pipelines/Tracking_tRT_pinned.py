98bda8819c8630a739959e900ba1945c0ba18d40
Meet Doshi <meet.doshi3196@gmail.com>
2026-03-17 01:02:55 +0530

    made Tracking_tRT_pinned work!

--- pipelines/Tracking_tRT_pinned.py
+++ pipelines/Tracking_tRT_pinned.py
@@ -11,6 +11,34 @@ fps_tally = deque(maxlen=100)
 cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
+from torchvision.transforms import v2
+
+# Define the pipeline once
+preprocess = v2.Compose([
+    # 1. Resize so the longest edge is 640 (maintains aspect ratio)
+    v2.Resize(size=640, max_size=641), 
+    
+    # 2. Pad or CenterCrop to reach exactly 640x640 with grey background
+    v2.CenterCrop(size=(640, 640)), 
+    
+    # 3. Convert to float and normalize
+    v2.ToDtype(torch.float32, scale=True), 
+])
+
+# import torchvision.transforms as T
+
+# preprocess = T.Compose([
+#     # 1. Resize so the LONGEST side is 640 (maintains aspect ratio)
+#     T.Resize(640, max_size=641), 
+    
+#     # 2. Add padding to make it exactly 640x640 (CenterCrop pads if image < size)
+#     T.CenterCrop(640), 
+    
+#     # 3. Standard normalization/conversion
+#     T.ToTensor(),
+# ])
+
+
 prev_time = time.time()
 
 while True:
@@ -22,16 +50,12 @@ while True:
     start = time.time()
 
     frame = cv2.cvtColor(frame0, cv2.COLOR_BGR2RGB)
-    frame = cv2.resize(frame, (640,640))
+    # frame = cv2.resize(frame, (640,640))
 
+    
     # numpy -> tensor
     frame_t = torch.from_numpy(frame)
 
-    # HWC → CHW
-    frame_t = frame_t.permute(2,0,1).float()/255.0
-
-    # add batch dimension
-    frame_t = frame_t.unsqueeze(0)
 
     # PIN MEMORY
     frame_t = frame_t.pin_memory()
@@ -39,12 +63,23 @@ while True:
     # async GPU transfer
     frame_t = frame_t.to("cuda", non_blocking=True)
 
+    # HWC → CHW
+    frame_t = frame_t.permute(2,0,1) #.float()/255.0
+
+    frame_t = preprocess(frame_t)
+
+    # add batch dimension
+    frame_t = frame_t.unsqueeze(0)
+
+
+
     # inference
     results = model.track(frame_t, verbose=False, tracker="bytetrack.yaml",
                             persist=True, conf=0.5, iou=0.5)
     end = time.time()
 
-    annotated_img = results[0].plot(img=frame0)
+    # results[0].orig_img = frame0
+    annotated_img = results[0].plot()
     # print(type(annotated_img))
 
     inf_time = (end - start)*1000
