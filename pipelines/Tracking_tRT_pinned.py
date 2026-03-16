8652d7ba8675e3451ee5a82d94b804bc2500a603
Meet Doshi <meet.doshi3196@gmail.com>
2026-03-17 03:52:01 +0530

    Preporcess modifications in tRT_pinned

--- pipelines/Tracking_tRT_pinned.py
+++ pipelines/Tracking_tRT_pinned.py
@@ -1,18 +1,39 @@
 import cv2
+import numpy as np
 import torch
 from ultralytics import YOLO
 import time
 from collections import deque
 
-model = YOLO("yolo26n.engine")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device detection
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = "cpu"
 
+print(f"""
+======> Using {device}
+""")
+
+# Model loading
+selected_model = "yolo26n.pt"
+# selected_model = "yolo26n.engine"
+# selected_model = "yolo26n-seg.pt"
+model = YOLO(selected_model)
+
+# perfromance measures
 fps_tally = deque(maxlen=100)
 
+#camera stream capturing
 cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
-from torchvision.transforms import v2
 
+# Preprocessing pipeline
+from torchvision.transforms import v2
 # Define the pipeline once
 preprocess = v2.Compose([
     # 1. Resize so the longest edge is 640 (maintains aspect ratio)
@@ -23,20 +44,11 @@ preprocess = v2.Compose([
     
     # 3. Convert to float and normalize
     v2.ToDtype(torch.float32, scale=True), 
+
+    # # Normalize for ImageNet Statistics
+    # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-# import torchvision.transforms as T
-
-# preprocess = T.Compose([
-#     # 1. Resize so the LONGEST side is 640 (maintains aspect ratio)
-#     T.Resize(640, max_size=641), 
-    
-#     # 2. Add padding to make it exactly 640x640 (CenterCrop pads if image < size)
-#     T.CenterCrop(640), 
-    
-#     # 3. Standard normalization/conversion
-#     T.ToTensor(),
-# ])
 
 
 prev_time = time.time()
@@ -55,27 +67,30 @@ while True:
     
     # numpy -> tensor
     frame_t = torch.from_numpy(frame)
-
-
-    # PIN MEMORY
-    frame_t = frame_t.pin_memory()
-
-    # async GPU transfer
-    frame_t = frame_t.to("cuda", non_blocking=True)
+    # PIN MEMORY # async GPU transfer
+    frame_t = frame_t.pin_memory().to(device=device, non_blocking=True)
 
     # HWC → CHW
-    frame_t = frame_t.permute(2,0,1) #.float()/255.0
+    frame_t = frame_t.permute(2,0,1)#.float()#/255.0
 
+    #Apply image regarding processing
     frame_t = preprocess(frame_t)
 
     # add batch dimension
     frame_t = frame_t.unsqueeze(0)
 
+    # # Normalize
+    # mean = torch.tensor([0.485, 0.456, 0.406], device=device).view(1, 3, 1, 1)
+    # std = torch.tensor([0.229, 0.224, 0.225], device=device).view(1, 3, 1, 1)
+    # frame_t = (frame_t - mean) / std
+
+    # print(frame_t.shape)
+
 
 
     # inference
     results = model.track(frame_t, verbose=False, tracker="bytetrack.yaml",
-                            persist=True, conf=0.5, iou=0.5)
+                            persist=True, conf=0.4, iou=0.5)
     end = time.time()
 
     # results[0].orig_img = frame0
@@ -110,5 +125,6 @@ while True:
     if cv2.waitKey(1) == ord("q"):
         break
 
+print(f"Average FPS of last 100 frames: {np.mean(np.array(fps_tally))}")
 cap.release()
 cv2.destroyAllWindows()
